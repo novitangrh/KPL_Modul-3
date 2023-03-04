a5725bfd83a8266b0bf9c7db9dcaf17b1887b9a3
novitangrh <novitasabila70@gmail.com>
2023-03-05 00:51:00 +0700

    tambahan fitur GUI

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,23 @@ namespace TPMOD3_1302213030_NovitaSabilaNugraha
         public Form1()
         {
             InitializeComponent();
+            textBox1.BackColor = Color.LightPink;
+            label1.BackColor = Color.HotPink;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Halo " + textBox1.Text;
         }
     }
 }
